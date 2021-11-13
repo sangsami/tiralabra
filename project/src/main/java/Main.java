501e9093bcb17b6032f54648fcdf151db0ab3f87
Sangsami <dinh.sami@gmail.com>
2021-11-13 16:50:53 +0200

    refactoring

---
+++ project/src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author samidinh
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your text to be encrypted");
+        String toEncrypt = scanner.nextLine();
+    }
+    
+}
